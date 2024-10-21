1be2da1e909994931be3194edb9355bdeceb35ca
M Wahhab Mirza - Vector Labs FZE LLC <144521746+MWahhabMirzaVectorLabsFZELLC@users.noreply.github.com>
2024-10-21 17:57:04 +0500

    Update GamePage.jsx

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -188,11 +188,11 @@ export default function Component({ setToken }) {
     // Vibration logic based on element type
     if (navigator.vibrate) {
       if (type === "fly") {
-        navigator.vibrate(1500); // Short vibration for catching a fly
+        navigator.vibrate(1100); // Short vibration for catching a fly
       } else if (type === "bomb") {
-        navigator.vibrate([1000, 700, 1000]); // Longer pattern for bombs
+        navigator.vibrate(1200); // Longer pattern for bombs
       } else if (type === "freezer") {
-        navigator.vibrate(1000); // Continuous vibration for freezer
+        navigator.vibrate(1110); // Continuous vibration for freezer
       } else if (type === "mysteryBox") {
         navigator.vibrate([300, 100, 300]); // Distinct pattern for mystery box
       }
